56a50876fa44fdf2ecf36ceec25f4e802157020c
made-devs <nmaderama@gmail.com>
2025-04-04 19:02:45 +0700

    Feat: Add Pic For Promo Impor (1-10)

---
+++ src/data/promoImpor.js
@@ -0,0 +1,226 @@
+// src/data/promoImpor.js
+
+const promoImpor = [
+  {
+    title: "FREE KONSULTASI BISNIS & ANALYSIS PRODUK EXPERT",
+    desc: "Sekilat sesuatu, biasanya berukuran kecil atau sedang, yang dikemas dan ditumpuk atau dikotakkan, parcel.",
+    desc_modal:
+      "Maksimalkan peluang bisnis Anda dengan Member Platinum PastiCuan! Dengan diskon spesial 84%, Anda bisa menikmati berbagai keuntungan eksklusif seperti FREE 35 Barang Impor Lintas Komoditas, 22 Promo Gratis Buyer Member, serta komisi penjualan, referral, dan cargo Platinum.",
+    image:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743767471/pasticuan/promo-impor/1.webp",
+    img_modal:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743767471/pasticuan/promo-impor/1.webp",
+  },
+  {
+    title: "NO PROFESIONAL FEE (KHUSUS BULAN INI)",
+    desc: "Sekilat sesuatu, biasanya berukuran kecil atau sedang, yang dikemas dan ditumpuk atau dikotakkan, parcel.",
+    desc_modal:
+      "Maksimalkan peluang bisnis Anda dengan Member Platinum PastiCuan! Dengan diskon spesial 84%, Anda bisa menikmati berbagai keuntungan eksklusif seperti FREE 35 Barang Impor Lintas Komoditas, 22 Promo Gratis Buyer Member, serta komisi penjualan, referral, dan cargo Platinum.",
+    image:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743767471/pasticuan/promo-impor/2.webp",
+    img_modal:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743767471/pasticuan/promo-impor/2.webp",
+  },
+  {
+    title: "FREE KONSULTAN BISNIS EXPERT LANJUTAN",
+    desc: "Sekilat sesuatu, biasanya berukuran kecil atau sedang, yang dikemas dan ditumpuk atau dikotakkan, parcel.",
+    desc_modal:
+      "Maksimalkan peluang bisnis Anda dengan Member Platinum PastiCuan! Dengan diskon spesial 84%, Anda bisa menikmati berbagai keuntungan eksklusif seperti FREE 35 Barang Impor Lintas Komoditas, 22 Promo Gratis Buyer Member, serta komisi penjualan, referral, dan cargo Platinum.",
+    image:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743767471/pasticuan/promo-impor/3.webp",
+    img_modal:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743767471/pasticuan/promo-impor/3.webp",
+  },
+  {
+    title: "CUSTOMER SERVICE 24/7",
+    desc: "Sekilat sesuatu, biasanya berukuran kecil atau sedang, yang dikemas dan ditumpuk atau dikotakkan, parcel.",
+    desc_modal:
+      "Maksimalkan peluang bisnis Anda dengan Member Platinum PastiCuan! Dengan diskon spesial 84%, Anda bisa menikmati berbagai keuntungan eksklusif seperti FREE 35 Barang Impor Lintas Komoditas, 22 Promo Gratis Buyer Member, serta komisi penjualan, referral, dan cargo Platinum.",
+    image:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743767471/pasticuan/promo-impor/4.webp",
+    img_modal:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743767471/pasticuan/promo-impor/4.webp",
+  },
+  {
+    title: "FREE VISIT SHOWROOM MELIHAT SAMPLE BARANG",
+    desc: "Sekilat sesuatu, biasanya berukuran kecil atau sedang, yang dikemas dan ditumpuk atau dikotakkan, parcel.",
+    desc_modal:
+      "Maksimalkan peluang bisnis Anda dengan Member Platinum PastiCuan! Dengan diskon spesial 84%, Anda bisa menikmati berbagai keuntungan eksklusif seperti FREE 35 Barang Impor Lintas Komoditas, 22 Promo Gratis Buyer Member, serta komisi penjualan, referral, dan cargo Platinum.",
+    image:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743767471/pasticuan/promo-impor/5.webp",
+    img_modal:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743767471/pasticuan/promo-impor/5.webp",
+  },
+  {
+    title: "PEMBUATAN LOGO BRAND (3X REVISI)",
+    desc: "Sekilat sesuatu, biasanya berukuran kecil atau sedang, yang dikemas dan ditumpuk atau dikotakkan, parcel.",
+    desc_modal:
+      "Maksimalkan peluang bisnis Anda dengan Member Platinum PastiCuan! Dengan diskon spesial 84%, Anda bisa menikmati berbagai keuntungan eksklusif seperti FREE 35 Barang Impor Lintas Komoditas, 22 Promo Gratis Buyer Member, serta komisi penjualan, referral, dan cargo Platinum.",
+    image:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743767471/pasticuan/promo-impor/7.webp",
+    img_modal:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743767471/pasticuan/promo-impor/7.webp",
+  },
+  {
+    title: "FREE 1 WEBSITE PREMIUM",
+    desc: "Sekilat sesuatu, biasanya berukuran kecil atau sedang, yang dikemas dan ditumpuk atau dikotakkan, parcel.",
+    desc_modal:
+      "Maksimalkan peluang bisnis Anda dengan Member Platinum PastiCuan! Dengan diskon spesial 84%, Anda bisa menikmati berbagai keuntungan eksklusif seperti FREE 35 Barang Impor Lintas Komoditas, 22 Promo Gratis Buyer Member, serta komisi penjualan, referral, dan cargo Platinum.",
+    image:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743767471/pasticuan/promo-impor/7.webp",
+    img_modal:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743767471/pasticuan/promo-impor/7.webp",
+  },
+  {
+    title: "PEMBUATAN STATER PACK & SET UP : INSTAGRAM / TIKTOK",
+    desc: "Sekilat sesuatu, biasanya berukuran kecil atau sedang, yang dikemas dan ditumpuk atau dikotakkan, parcel.",
+    desc_modal:
+      "Maksimalkan peluang bisnis Anda dengan Member Platinum PastiCuan! Dengan diskon spesial 84%, Anda bisa menikmati berbagai keuntungan eksklusif seperti FREE 35 Barang Impor Lintas Komoditas, 22 Promo Gratis Buyer Member, serta komisi penjualan, referral, dan cargo Platinum.",
+    image:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743767471/pasticuan/promo-impor/8.webp",
+    img_modal:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743767471/pasticuan/promo-impor/8.webp",
+  },
+  {
+    title: "PEMBUATAN COPYWRITING PROFESIONAL",
+    desc: "Sekilat sesuatu, biasanya berukuran kecil atau sedang, yang dikemas dan ditumpuk atau dikotakkan, parcel.",
+    desc_modal:
+      "Maksimalkan peluang bisnis Anda dengan Member Platinum PastiCuan! Dengan diskon spesial 84%, Anda bisa menikmati berbagai keuntungan eksklusif seperti FREE 35 Barang Impor Lintas Komoditas, 22 Promo Gratis Buyer Member, serta komisi penjualan, referral, dan cargo Platinum.",
+    image:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743767471/pasticuan/promo-impor/9.webp",
+    img_modal:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743767471/pasticuan/promo-impor/9.webp",
+  },
+  {
+    title: "PEMBUATAN KONSEP MARKETPLACE",
+    desc: "Sekilat sesuatu, biasanya berukuran kecil atau sedang, yang dikemas dan ditumpuk atau dikotakkan, parcel.",
+    desc_modal:
+      "Maksimalkan peluang bisnis Anda dengan Member Platinum PastiCuan! Dengan diskon spesial 84%, Anda bisa menikmati berbagai keuntungan eksklusif seperti FREE 35 Barang Impor Lintas Komoditas, 22 Promo Gratis Buyer Member, serta komisi penjualan, referral, dan cargo Platinum.",
+    image:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743767471/pasticuan/promo-impor/10.webp",
+    img_modal:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743767471/pasticuan/promo-impor/10.webp",
+  },
+  {
+    title: "PEMBUATAN SAMPLE KONTEN PROFESIONAL",
+    desc: "Sekilat sesuatu, biasanya berukuran kecil atau sedang, yang dikemas dan ditumpuk atau dikotakkan, parcel.",
+    desc_modal:
+      "Maksimalkan peluang bisnis Anda dengan Member Platinum PastiCuan! Dengan diskon spesial 84%, Anda bisa menikmati berbagai keuntungan eksklusif seperti FREE 35 Barang Impor Lintas Komoditas, 22 Promo Gratis Buyer Member, serta komisi penjualan, referral, dan cargo Platinum.",
+    image:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743763718/pasticuan/reward-member/6.webp",
+    img_modal:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743763718/pasticuan/reward-member/6.webp",
+  },
+  {
+    title: "PEMBUATAN SAMPLE KATALOG PRODUK",
+    desc: "Sekilat sesuatu, biasanya berukuran kecil atau sedang, yang dikemas dan ditumpuk atau dikotakkan, parcel.",
+    desc_modal:
+      "Maksimalkan peluang bisnis Anda dengan Member Platinum PastiCuan! Dengan diskon spesial 84%, Anda bisa menikmati berbagai keuntungan eksklusif seperti FREE 35 Barang Impor Lintas Komoditas, 22 Promo Gratis Buyer Member, serta komisi penjualan, referral, dan cargo Platinum.",
+    image:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743763718/pasticuan/reward-member/6.webp",
+    img_modal:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743763718/pasticuan/reward-member/6.webp",
+  },
+  {
+    title: "PEMBUATAN LANDING PAGE BISNIS",
+    desc: "Sekilat sesuatu, biasanya berukuran kecil atau sedang, yang dikemas dan ditumpuk atau dikotakkan, parcel.",
+    desc_modal:
+      "Maksimalkan peluang bisnis Anda dengan Member Platinum PastiCuan! Dengan diskon spesial 84%, Anda bisa menikmati berbagai keuntungan eksklusif seperti FREE 35 Barang Impor Lintas Komoditas, 22 Promo Gratis Buyer Member, serta komisi penjualan, referral, dan cargo Platinum.",
+    image:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743763718/pasticuan/reward-member/6.webp",
+    img_modal:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743763718/pasticuan/reward-member/6.webp",
+  },
+  {
+    title: "PEMBUATAN TEXT WA BISNIS",
+    desc: "Sekilat sesuatu, biasanya berukuran kecil atau sedang, yang dikemas dan ditumpuk atau dikotakkan, parcel.",
+    desc_modal:
+      "Maksimalkan peluang bisnis Anda dengan Member Platinum PastiCuan! Dengan diskon spesial 84%, Anda bisa menikmati berbagai keuntungan eksklusif seperti FREE 35 Barang Impor Lintas Komoditas, 22 Promo Gratis Buyer Member, serta komisi penjualan, referral, dan cargo Platinum.",
+    image:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743763718/pasticuan/reward-member/6.webp",
+    img_modal:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743763718/pasticuan/reward-member/6.webp",
+  },
+  {
+    title: "PEMBUATAN GOOGLE MAPS + GALLERY PROFESIONAL",
+    desc: "Sekilat sesuatu, biasanya berukuran kecil atau sedang, yang dikemas dan ditumpuk atau dikotakkan, parcel.",
+    desc_modal:
+      "Maksimalkan peluang bisnis Anda dengan Member Platinum PastiCuan! Dengan diskon spesial 84%, Anda bisa menikmati berbagai keuntungan eksklusif seperti FREE 35 Barang Impor Lintas Komoditas, 22 Promo Gratis Buyer Member, serta komisi penjualan, referral, dan cargo Platinum.",
+    image:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743763718/pasticuan/reward-member/6.webp",
+    img_modal:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743763718/pasticuan/reward-member/6.webp",
+  },
+  {
+    title: "COACHING DIGITAL MARKETING EXPERT",
+    desc: "Sekilat sesuatu, biasanya berukuran kecil atau sedang, yang dikemas dan ditumpuk atau dikotakkan, parcel.",
+    desc_modal:
+      "Maksimalkan peluang bisnis Anda dengan Member Platinum PastiCuan! Dengan diskon spesial 84%, Anda bisa menikmati berbagai keuntungan eksklusif seperti FREE 35 Barang Impor Lintas Komoditas, 22 Promo Gratis Buyer Member, serta komisi penjualan, referral, dan cargo Platinum.",
+    image:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743763718/pasticuan/reward-member/6.webp",
+    img_modal:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743763718/pasticuan/reward-member/6.webp",
+  },
+  {
+    title: "COACHING TAX",
+    desc: "Sekilat sesuatu, biasanya berukuran kecil atau sedang, yang dikemas dan ditumpuk atau dikotakkan, parcel.",
+    desc_modal:
+      "Maksimalkan peluang bisnis Anda dengan Member Platinum PastiCuan! Dengan diskon spesial 84%, Anda bisa menikmati berbagai keuntungan eksklusif seperti FREE 35 Barang Impor Lintas Komoditas, 22 Promo Gratis Buyer Member, serta komisi penjualan, referral, dan cargo Platinum.",
+    image:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743763718/pasticuan/reward-member/6.webp",
+    img_modal:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743763718/pasticuan/reward-member/6.webp",
+  },
+  {
+    title: "COACHING HUKUM",
+    desc: "Sekilat sesuatu, biasanya berukuran kecil atau sedang, yang dikemas dan ditumpuk atau dikotakkan, parcel.",
+    desc_modal:
+      "Maksimalkan peluang bisnis Anda dengan Member Platinum PastiCuan! Dengan diskon spesial 84%, Anda bisa menikmati berbagai keuntungan eksklusif seperti FREE 35 Barang Impor Lintas Komoditas, 22 Promo Gratis Buyer Member, serta komisi penjualan, referral, dan cargo Platinum.",
+    image:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743763718/pasticuan/reward-member/6.webp",
+    img_modal:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743763718/pasticuan/reward-member/6.webp",
+  },
+  {
+    title: "COACHING ADS / IKLAN",
+    desc: "Sekilat sesuatu, biasanya berukuran kecil atau sedang, yang dikemas dan ditumpuk atau dikotakkan, parcel.",
+    desc_modal:
+      "Maksimalkan peluang bisnis Anda dengan Member Platinum PastiCuan! Dengan diskon spesial 84%, Anda bisa menikmati berbagai keuntungan eksklusif seperti FREE 35 Barang Impor Lintas Komoditas, 22 Promo Gratis Buyer Member, serta komisi penjualan, referral, dan cargo Platinum.",
+    image:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743763718/pasticuan/reward-member/6.webp",
+    img_modal:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743763718/pasticuan/reward-member/6.webp",
+  },
+  {
+    title: "COACHING SEO BASIC",
+    desc: "Sekilat sesuatu, biasanya berukuran kecil atau sedang, yang dikemas dan ditumpuk atau dikotakkan, parcel.",
+    desc_modal:
+      "Maksimalkan peluang bisnis Anda dengan Member Platinum PastiCuan! Dengan diskon spesial 84%, Anda bisa menikmati berbagai keuntungan eksklusif seperti FREE 35 Barang Impor Lintas Komoditas, 22 Promo Gratis Buyer Member, serta komisi penjualan, referral, dan cargo Platinum.",
+    image:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743763718/pasticuan/reward-member/6.webp",
+    img_modal:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743763718/pasticuan/reward-member/6.webp",
+  },
+  {
+    title: "COACHING CONTENT CREATOR EXPERT",
+    desc: "Sekilat sesuatu, biasanya berukuran kecil atau sedang, yang dikemas dan ditumpuk atau dikotakkan, parcel.",
+    desc_modal:
+      "Maksimalkan peluang bisnis Anda dengan Member Platinum PastiCuan! Dengan diskon spesial 84%, Anda bisa menikmati berbagai keuntungan eksklusif seperti FREE 35 Barang Impor Lintas Komoditas, 22 Promo Gratis Buyer Member, serta komisi penjualan, referral, dan cargo Platinum.",
+    image:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743763718/pasticuan/reward-member/6.webp",
+    img_modal:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743763718/pasticuan/reward-member/6.webp",
+  },
+  {
+    title: "FREE MENJADI MEMBERSHIP PASTICUAN.CO.ID",
+    desc: "Sekilat sesuatu, biasanya berukuran kecil atau sedang, yang dikemas dan ditumpuk atau dikotakkan, parcel.",
+    desc_modal:
+      "Maksimalkan peluang bisnis Anda dengan Member Platinum PastiCuan! Dengan diskon spesial 84%, Anda bisa menikmati berbagai keuntungan eksklusif seperti FREE 35 Barang Impor Lintas Komoditas, 22 Promo Gratis Buyer Member, serta komisi penjualan, referral, dan cargo Platinum.",
+    image:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743763718/pasticuan/reward-member/6.webp",
+    img_modal:
+      "https://res.cloudinary.com/df7lzicrz/image/upload/v1743763718/pasticuan/reward-member/6.webp",
+  },
+];
+
+export default promoImpor;
